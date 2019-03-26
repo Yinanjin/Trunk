bea5d902e0dbea73f9b285e6171f21515b177813
Yinanjin <37734938+Yinanjin@users.noreply.github.com>
2019-03-27 04:12:47 +1100

    Add files via upload

---
+++ Models/Inventory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Trunk.Models
+{
+    public class Inventory
+    {
+        private string _colour;
+
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Colour { get => _colour; set => _colour = value; }
+        public decimal Price { get; set; }
+    }
+}
