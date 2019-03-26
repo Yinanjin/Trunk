9264282574cfd81b7d8e896d68b80df123554937
Yinanjin <37734938+Yinanjin@users.noreply.github.com>
2019-03-27 04:10:49 +1100

    Add files via upload

---
+++ Data/TrunkContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Trunk.Models
+{
+    public class TrunkContext : DbContext
+    {
+        public TrunkContext (DbContextOptions<TrunkContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Trunk.Models.Inventory> Inventory { get; set; }
+    }
+}
